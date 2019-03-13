aa67092e2be7d8c22249987bcb0e4fbfdafe44e8
Mikle <bigrichman7@gmail.com>
2019-03-13 22:55:20 +0300

    Initial commit

---
+++ app/src/main/java/com/bigrichman/android/sunset/SunsetActivity.java
@@ -0,0 +1,12 @@
+package com.bigrichman.android.sunset;
+
+import android.support.v4.app.Fragment;
+
+public class SunsetActivity extends SingleFragmentActivity {
+
+    @Override
+    public Fragment createFragment() {
+        return SunsetFragment.newInstance();
+    }
+
+}
